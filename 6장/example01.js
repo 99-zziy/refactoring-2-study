81d0e3dbcb20e9fee19136afbf5180d4dcae375f
99-zziy <wldud8229@gmail.com>
2022-01-27 21:07:44 +0900

    리팩터링 4단계

--- 6장/example01.js
+++ 6장/example01.js
@@ -2,11 +2,11 @@
 리팩터링을 할 때 절차를 쪼개는 이유는? -> 리팩터링 과정중 겉보기 동작을 유지하기 위함
 */
 
-// 3단계 -> 겉보기 동작 유지, 코드 깨짐 없음
-function xxNewEngland(stateCode) {
+// 4단계 -> 겉보기 동작 유지, 코드 깨짐 없음
+function inNewEngland(stateCode) {
   return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
 }
 
 const newEnglanders = someCostomers.filter((c) =>
-  xxNewEngland(aCustomer.address.state)
+  inNewEngland(aCustomer.address.state)
 );
