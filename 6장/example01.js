640490b5fe55fd85fbad47680061845a2b1a04ff
99-zziy <wldud8229@gmail.com>
2022-01-27 21:06:33 +0900

    리팩터링 1단계

--- 6장/example01.js
+++ 6장/example01.js
@@ -2,7 +2,11 @@
 리팩터링을 할 때 절차를 쪼개는 이유는? -> 리팩터링 과정중 겉보기 동작을 유지하기 위함
 */
 
-// 리팩터링 하기전 코드
+// 1단계 -> 겉보기 동작 유지, 코드 깨짐 없음
+function xxNewEngland(stateCode) {
+  return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
+}
+
 function inNewEngland(aCustomer) {
   return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(aCustomer.address.state);
 }
